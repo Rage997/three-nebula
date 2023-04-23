a662e2d043433e31c97e7153401ea964060273cb
Niccolo Zuppichini <nico@mndcr.com>
2023-04-23 09:46:10 +0200

    Refactor SphereZone

--- src/zone/SphereZone.js
+++ src/zone/SphereZone.js
@@ -18,33 +18,13 @@ export default class SphereZone extends Zone {
    * @param {number} radius - the sphere's radius value
    * @return void
    */
-  constructor(centerX, centerY, centerZ, radius) {
+  constructor(centerX = 0, centerY = 0, centerZ = 0, radius = 100) {
     super(type);
-
-    // TODO see below, these should probably be assigned properly
-    // eslint-disable-next-line
-    let x, y, z, r;
-
-    if (Util.isUndefined(centerY, centerZ, radius)) {
-      x = y = z = 0;
-      r = centerX || 100;
-    } else {
-      x = centerX;
-      // eslint-disable-next-line
-      y = centerY;
-      // eslint-disable-next-line
-      z = centerZ;
-      r = radius;
-    }
-
-    this.x = x;
-
-    // TODO shouldn't this be set to y?
-    this.y = x;
-
-    // TODO shouldn't this be set to z?
-    this.z = x;
-    this.radius = r;
+  
+    this.x = centerX;
+    this.y = centerY;
+    this.z = centerZ;
+    this.radius = radius;
     this.the = this.phi = 0;
   }
 
@@ -77,42 +57,32 @@ export default class SphereZone extends Zone {
   _cross() {
     console.warn(`${this.constructor.name} does not support the _cross method`);
   }
-}
 
-SphereZone.prototype.getPosition = (function() {
-  var tha, phi, r;
-
-  return function() {
-    this.random = Math.random();
-
-    r = this.random * this.radius;
-    tha = PI * Math.random(); //[0-pi]
-    phi = PI * 2 * Math.random(); //[0-2pi]
-
+  getPosition() {
+    const r = Math.random() * this.radius;
+    const tha = Math.PI * Math.random(); //[0-pi]
+    const phi = Math.PI * 2 * Math.random(); //[0-2pi]
+  
     this.vector.x = this.x + r * Math.sin(tha) * Math.cos(phi);
     this.vector.y = this.y + r * Math.sin(phi) * Math.sin(tha);
     this.vector.z = this.z + r * Math.cos(tha);
-
+  
     return this.vector;
-  };
-})();
-
-SphereZone.prototype._bound = (function() {
-  var normal = new Vector3D(),
-    v = new Vector3D(),
-    k;
-
-  return function(particle) {
-    var d = particle.position.distanceTo(this);
-
+  }
+  
+  _bound(particle) {
+    const normal = new Vector3D();
+    const v = new Vector3D();
+    const d = particle.position.distanceTo(this);
+  
     if (d + particle.radius >= this.radius) {
       normal
         .copy(particle.position)
         .sub(this)
         .normalize();
       v.copy(particle.velocity);
-      k = 2 * v.dot(normal);
+      const k = 2 * v.dot(normal);
       particle.velocity.sub(normal.scalar(k));
     }
-  };
-})();
+  }
+}
