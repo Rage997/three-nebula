207b77f6adf670d855597e25ac6c11046dc3632b
Niccolo Zuppichini <nico@mndcr.com>
2023-04-23 09:27:59 +0200

    Dropped deprecated THREE.Geometry and use THREE.BufferGeometry

--- src/zone/MeshZone.js
+++ src/zone/MeshZone.js
@@ -1,27 +1,27 @@
 import Zone from './Zone';
 import { ZONE_TYPE_MESH as type } from './types';
+import { Vector3 } from 'three';
 
 /**
- * Uses a three THREE.Geometry to determine the zone parameters.
+ * Uses a three THREE.BufferGeometry to determine the zone parameters.
  *
  */
 export default class MeshZone extends Zone {
   /**
    * @constructs {MeshZone}
    *
-   * @param {THREE.Geometry|Mesh} bounds - the geometry or mesh that will determine the zone bounds
+   * @param {THREE.BufferGeometry|Mesh} bounds - the geometry or mesh that will determine the zone bounds
    * @param {number} scale - the zone scale
-   * @param {THREE.Geometry} ThreeGeometry - the three geometry class
    * @return void
    */
-  constructor(bounds, scale = 1, ThreeGeometry) {
+  constructor(bounds, scale = 1) {
     super(type);
 
     this.geometry = null;
     this.scale = scale;
     this.supportsCrossing = false;
 
-    if (bounds.type && bounds.type === 'Geometry') {
+    if (bounds.type && bounds.type === 'BufferGeometry') {
       this.geometry = bounds;
     }
 
@@ -33,10 +33,6 @@ export default class MeshZone extends Zone {
       throw new Error(
         'MeshZone unable to set geometry from the supplied bounds'
       );
-    }
-
-    if (this.geometry.isBufferGeometry) {
-      this.geometry = new ThreeGeometry().fromBufferGeometry(this.geometry);
     }
   }
 
@@ -50,12 +46,14 @@ export default class MeshZone extends Zone {
   }
 
   getPosition() {
-    const vertices = this.geometry.vertices;
-    const rVector = vertices[(vertices.length * Math.random()) >> 0];
+    const positionAttribute = this.geometry.getAttribute('position');
+    const randomIndex = (positionAttribute.count * Math.random()) >> 0;
 
-    this.vector.x = rVector.x * this.scale;
-    this.vector.y = rVector.y * this.scale;
-    this.vector.z = rVector.z * this.scale;
+    const x = positionAttribute.getX(randomIndex) * this.scale;
+    const y = positionAttribute.getY(randomIndex) * this.scale;
+    const z = positionAttribute.getZ(randomIndex) * this.scale;
+
+    this.vector = new Vector3(x, y, z);
 
     return this.vector;
   }
